08153db09e73767ce8dd71473d2a70d84f194568
jmhockham <jmhockham@gmail.com>
2019-02-27 11:53:40 +0000

    Futures and handling their onComplete's

---
+++ src/main/scala/FutureNamePrint.scala
@@ -0,0 +1,40 @@
+import scala.concurrent.Future
+import scala.util.{Failure, Success}
+
+object FutureNamePrint extends App {
+
+  import scala.concurrent.ExecutionContext.Implicits.global
+
+  val fTomName: Future[String] = Future {
+    "Thomas"
+  }
+  val fDickName: Future[String] = Future {
+    "Richard"
+  }
+  val fHarryName: Future[String] = Future {
+    "Harrold"
+  }
+
+  fTomName.onComplete{
+    case Success(name) => println("completed: "+name)
+    case Failure(t) => println("error when getting name:"+t.getLocalizedMessage)
+  }
+  fDickName.onComplete{
+    case Success(name) => println("completed: "+name)
+    case Failure(t) => println("error when getting name:"+t.getLocalizedMessage)
+  }
+  fHarryName.onComplete{
+    case Success(name) => println("completed: "+name)
+    case Failure(t) => println("error when getting name:"+t.getLocalizedMessage)
+  }
+
+  fTomName foreach {
+    name => println(s"for loop: $name")
+  }
+
+  private val eventualString: Future[String] = for {
+    t <- fTomName
+  } yield t
+  eventualString
+
+}
